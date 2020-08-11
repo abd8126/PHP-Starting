8cdd49b99aa665d3dd11a718e1cd818f8bc35eba
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-11 12:46:25 +0530

    Lecture 11

---
+++ lecture11.php
@@ -0,0 +1,13 @@
+<?php
+$inta =128;
+$intb =100128;
+$intc =128e5;
+$txt ='Anubhav';
+$x =5.89e5;
+var_dump($inta);
+var_dump($intb);
+var_dump($intc);
+var_dump($txt);
+var_dump($x);
+echo ("Anubhav");
+?>
---
+++ lecture11b.php
@@ -0,0 +1,12 @@
+<?php
+$inte = 128;
+$into = 10200;
+$b  = '1b106';
+$txt = "Anubhav bhardwaj";
+$x = 1.95e7;
+var_dump($inte);
+var_dump($into);
+var_dump($b);
+var_dump($txt);
+var_dump($x);
+?>
