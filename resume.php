72dcf578d26115fb21bc6da42b7428526982ffd2
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-16 11:58:56 +0530

    Resume for Amit Assignment

---
+++ resume.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<link rel="stylesheet"  type="text/css">
+<body style="background-image: url('https://raw.githubusercontent.com/abd8126/PHP-Starting/master/resume.jpg')">
+<div class="conatiner yes" style="text-align: center">
+        <h1>
+            <?php
+            echo "RESUME"
+            ?>
+            <hr width="100%" color="brown"/>
+        </h1>
+</div>
+<div style="text-align: center">
+            <?php
+            echo"<h2>PROFILE</h2>";
+            echo"<p>NAME:Amit Kumar Chaudhary</br> ROLL NO: 181500085</br>DEGREE: BTECH CS</br>PHONE:+91 89228 21202</br>ADDRESS: Gorakhpur</p>";
+            ?>
+    <hr width="35%" color="black"/>
+</div>
+<div class="container" id="yup" style="text-align: center">
+            <h2>
+                <?php
+                echo "EDUCATION QUALIFICATION";
+                ?>
+            </h2>
+            <hr width=18% color="black"/>
+    <?php
+    echo"<p>Pursing B.Tech From GLA University from 2018-2022</br> 12th from UP Board by 78.8%</br>10th from CBSE Board by 79%</p>";
+    ?>
+</div>
+<div class="container uu" style="text-align: center">
+            <h2>
+                <?php
+                echo "OBJECTIVE";
+                ?>
+            </h2>
+            <hr width=10% color="black"/>
+    <?php
+    echo"A Successful Person";
+    ?>
+</div>
+<div class="container" style="text-align: center">
+            <h2>
+                <?php
+                print"SKILL";
+                ?>
+            </h2>
+            <hr width=8% color="black"/>
+    <?php
+    echo"Coding in Python ,Java and C</br>Good Communication skill</br>Basic IOT Knowledge </br> Team Management";
+    ?>
+</div>
+<div class="container" style="text-align: center">
+            <h2>
+                <?php
+                echo "CERTIFICATES";
+                ?>
+            </h2>
+            <hr width=12% color="black"/>
+    <?php
+    echo" <br> Google IT Professional";
+    echo"<br> Introduction to HTML from Coursera";
+    echo "<br> Android from Udacity(running)";
+    echo "<br> Smart Water Management project in IOT";
+    echo" <br> Python for Everybody from Coursera";
+    ?>
+</div>
+<div class="container" style="text-align: center">
+            <h2>
+                <?php
+                echo"HOBBIES";?>
+            </h2>
+            <hr width=9% color="black"/>
+    <?php
+    echo" <br> Writing";
+    echo "<br>Reading Novels";
+    echo "<br>Playing Games";
+    ?>
+</div>
+</body>
