4721d92db99b87c6ed8e8fd3983d0a41cd624729
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-18 17:18:47 +0530

    PHP Assignment 2 Complete

---
+++ assignment2.php
@@ -0,0 +1,23 @@
+<?php
+$l = 5;
+$b = 6;
+$pi = 3.14;
+echo 'Area of Square= side * Side';
+echo '<br>';
+echo 'Side = 5';
+echo '<br>';
+echo ('Area Of Square is:'. $l*$l);
+echo '<br>';
+echo '<br>';
+echo 'Lenght = 5';
+echo '<br>';
+echo 'Breadth = 6';
+echo '<br>';
+echo ('Area Of Rectangle is:'. $l*$b);
+echo '<br>';
+echo '<br>';
+echo ('Are of Cicile is 2*3.14*lenght');
+echo '<br>';
+echo ('Area Of Circleis:'. $pi*$l*$l);
+
+?>
