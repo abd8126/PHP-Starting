d0a78ebd4d5629b30d5684dc74c4f2dc2ea3f5e3
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-23 23:29:02 +0530

    PHP LAB 5

---
+++ PHP LAB 5.php
@@ -0,0 +1,13 @@
+<?php
+$num = 56;
+var_dump($num);
+$str = (array)$num;
+var_dump($str);
+$boole = (bool)$boole;
+var_dump($boole);
+$txt = '78 naem';
+var_dump($txt);
+$itname = (int)$txt;
+var_dump($itname);
+echo (pi());
+?>
