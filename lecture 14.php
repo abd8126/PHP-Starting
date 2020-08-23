8d3c6929814ac12a0dee4055f55ddd701b705230
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-23 23:28:35 +0530

    Lecture 14

---
+++ lecture 14.php
@@ -0,0 +1,21 @@
+<?php
+$a = 'This is for Testing of Strpos Function';
+$b = '8126403847 is my mobile no.';
+$c = 'This is Anubhav Bhardwaj';
+echo strpos($a , 'Testing');
+echo '<br>';
+echo ucfirst($c);
+echo '<br>';
+echo ucfirst($b);
+echo '<br>';
+echo ucwords($c);
+echo '<hr>';
+$num1 = 2;
+$num2 = 4;
+$num3 = 'Anubhav';
+var_dump($num1);
+var_dump($num2);
+var_dump($num2/$num1);
+var_dump($num1/$num2);
+var_dump($num1+$num3);
+?>
