f2150e64fd277a16f7bfa200285a42790eb66294
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-24 13:19:42 +0530

    Lecture 15

---
+++ Lecture 15.php
@@ -0,0 +1,24 @@
+<?php
+$l = 5;
+$b = 6;
+pi();
+echo 'Area of Square= side * Side';
+echo '<br>';
+echo 'Side = 5';
+echo '<br>';
+echo ('Area Of Square is:'. $l*$l);
+echo '<br>';
+echo '<br>';
+echo 'Area Of Rectangle = Length * Breadth';
+echo '<br>';
+echo 'Lenght = 5';
+echo '<br>';
+echo 'Breadth = 6';
+echo '<br>';
+echo ('Area Of Rectangle is:'. $l*$b);
+echo '<br>';
+echo '<br>';
+echo ('Are of Cicile is pi*lenght');
+echo '<br>';
+echo ('Area Of Circleis:'. pi()*$l*$l);
+?>
