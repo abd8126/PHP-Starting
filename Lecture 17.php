a8ea6ecbc3842ceb489f9635a6c3a7344d872d2e
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-29 12:55:38 +0530

    Lecture 17

---
+++ Lecture 17.php
@@ -0,0 +1,13 @@
+<?php
+$num1= 21.0;
+$num2= 20;
+var_dump($num1==$num2);
+var_dump($num1===$num2);
+var_dump($num1!=$num2);
+var_dump($num1<>$num2);
+var_dump($num1!==$num2);
+var_dump($num1>$num2);
+var_dump($num1<$num2);
+var_dump($num1<=$num2);
+var_dump($num1<=>$num2);
+?>
