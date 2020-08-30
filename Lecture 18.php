70debef30d2ce1d37af542d457ba93fc94f9e299
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-08-30 22:52:38 +0530

    Lecture 18

---
+++ Lecture 18.php
@@ -0,0 +1,11 @@
+<?php
+$arr1 = array('a'=>1,'b'=>2,'c'=>3,'d'=>1,);
+$arr2 = array(5,6,7,8);
+var_dump($arr1);
+var_dump($arr2);
+var_dump($arr1+$arr2);//
+print_r($arr1+$arr2);
+var_dump($arr1==$arr2);
+var_dump($arr1===$arr2);
+var_dump($arr1!=$arr2);
+?>
