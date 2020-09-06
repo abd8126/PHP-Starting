b6f138785d6b648488ce288ffab69fd41621d793
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-09-06 22:44:43 +0530

    Lecture 20 If-else Statement

---
+++ lecture 20.php
@@ -0,0 +1,13 @@
+<?php
+$marks = 39;
+if($marks > 40){
+    echo "Marks of Student is : $marks";
+    echo "<br>";
+    echo 'Pass';
+}
+else{
+    echo 'Sorry';
+    echo '<br>';
+    echo 'You are Fail';
+}
+?>
