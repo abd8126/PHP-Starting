c7c843299db54703cb5420adb24a8987ea90774a
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-09-06 23:36:24 +0530

    PHP LAb 7

---
+++ PHP Lab 7.php
@@ -0,0 +1,40 @@
+<?php
+echo 'Enter Your Subject Mark and get Your Grade';
+$mark = 20;
+if ($mark<=10){
+    echo '<br>';
+    echo 'Grade I';
+}
+elseif ($mark<=20) {
+    echo '<br>';
+    echo 'Grade H';
+}
+elseif ($mark<=30) {
+    echo '<br>';
+    echo 'Grade G';
+}
+elseif ($mark<=40) {
+    echo '<br>';
+    echo 'Grade F';
+}
+elseif ($mark<=50) {
+    echo '<br>';
+    echo 'Grade E';
+}
+elseif ($mark<=60) {
+    echo '<br>';
+    echo 'Grade D';
+}
+elseif ($mark<=70) {
+    echo '<br>';
+    echo 'Grade C';
+}
+elseif ($mark<=80) {
+    echo '<br>';
+    echo 'Grade B';
+}
+elseif ($mark<=90) {
+    echo '<br>';
+    echo 'Grade A';
+}
+?>
