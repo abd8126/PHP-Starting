7b846a98adcf01468ccdec3f36303a4335fbf2c3
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-10-31 20:23:07 +0530

    Add files via upload
    Write a PHP Script to print all alphabets from a to z. - using while loop

---
+++ Assignment 5 Php Loops/4.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title> a to z</title>
+</head>
+<body bgcolor="Orange">
+<h1 align="center" >Print a to z</h1>
+<?php
+$range='a';
+$n=1;
+while($n<=26)
+{
+    echo "$range ";
+    $range++;
+    $n++;
+}
+
+?>
+</body>
+</html>
