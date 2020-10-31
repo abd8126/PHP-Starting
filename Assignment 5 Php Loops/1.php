6d8a640b809a575c5273fb2cb499e94063bde36e
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2020-10-31 20:20:29 +0530

    Assignment 5

---
+++ Assignment 5 Php Loops/1.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <title> Table in HTML </title>
+</head>
+<body background="abc.jpg">
+<h3 style="color: white"; > Q1.  Explain the concept of Nested table , Row span and colspan attributes in HTML 5, Create a table for the following data i.e PRODUCT NAME , PRODUCT ID, PRODUCT QUANTITY, PRODUCT PRICE. in last you have to show a TOTAL AMOUNT IN COLSPAN.</h3>
+<table border="4" cellpadding="5" cellspacing="5">
+    <tr>
+        <th>Product Name </th>
+        <th> Product ID </th>
+        <th>Product Quantity </th>
+        <th> Product Price</th>
+    </tr>
+    <tr>
+        <td> Mobile</td>
+        <td> 656465255 </td>
+        <td> 2</td>
+        <td> 99999</td>
+    </tr>
+    <tr>
+        <td> HeadPhone</td>
+        <td> 181500085 </td>
+        <td> 2</td>
+        <td> 200.0</td>
+    </tr>
+    <tr>
+        <td> Laptop</td>
+        <td> hp </td>
+        <td> 2</td>
+        <td> 999999</td>
+    </tr>
+    <tr >
+        <td colspan="5"> Total Amount : </td>
+    </tr>
+</table>
+</body>
+</html>
